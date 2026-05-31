d9fc7bde387c0b40fd884f5e379764e2756c7797
ayyldzirfan-byte <ayyldzirfan@gmail.com>
2026-06-01 02:13:37 +0300

    Add files via upload
    son guncelleme damgasi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kasa-prototype-v4";
+const CACHE_NAME = "kasa-prototype-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
